71db415840fa6adaa7595009616a93ec346d778f
Paul Dangelo <pdangelo1988@gmail.com>
2023-07-26 20:51:51 -0400

    form added. Working on API endpoint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
-import Image from 'next/image'
+"use client"
+
+import { useState, FormEvent } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const [inputVal, setInputVal] = useState("");
+  const { push } = useRouter();
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault()
+    push(`/prediction/${inputVal}`)
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  )
+    <div>
+      <div>
+        <h1> Enter your name </h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Type your name..."
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+        />
+        <button type="submit">Predict Data</button>
+      </form>
+    </div>
+  );
 }
